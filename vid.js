b980e199c1c3f6ad9c76cb60b696f34c8ee1d5ad
Avi Rana <68737072+Avi-Rana-1718@users.noreply.github.com>
2022-07-11 12:51:01 +0530

    Update vid.js

--- vid.js
+++ vid.js
@@ -147,8 +147,8 @@ if (vid.paused) {
     detailDiv.style.display="none";
     pauseBtn.style.display="block";
     bottomDiv.style.display="block";
-clearTimeout();
-var timeout = setInterval(hide,5000);
+var timeout = setTimeout(hide,5000);
+  clearTimeout();
 }
 detailDiv.style.display ="block";
 optionDiv.style.display ="block";
